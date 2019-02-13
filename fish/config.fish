5707ccb9a85bbf54b2e01ea0c0228eef62b96edf
Kyle Lanchman <k.lanchman@gmail.com>
2019-02-13 13:25:16 -0500

    Add Homebrew configuration

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,6 @@
 source ~/.config/fish/abbreviations.fish
 source ~/.config/fish/aliases.fish
+source ~/.brewconfig
+
 nvm use default > /dev/null
 status --is-interactive; and source (rbenv init -|psub)
