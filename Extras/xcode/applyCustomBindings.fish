ac286dec9bc6ddf6616bcc74c081a1fa5c81b7ee
Kyle Lanchman <k.lanchman@gmail.com>
2020-08-26 10:42:38 -0400

    Update custom Xcode keybindings
    Duplicate up/down multiple lines and highlight the duplicated block.
    Matches VS Code behavior.

--- Extras/xcode/applyCustomBindings.fish
+++ Extras/xcode/applyCustomBindings.fish
@@ -1,7 +1,15 @@
 #!/usr/bin/env fish
 
 set XCODE_KEYBINDINGS_FILE "/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources/IDETextKeyBindingSet.plist"
+set XCODE_BETA_KEYBINDINGS_FILE "/Applications/Xcode-beta.app/Contents/Frameworks/IDEKit.framework/Resources/IDETextKeyBindingSet.plist"
 set CUSTOM_KEYBINDINGS_FILE (pwd)/Extras/xcode/CustomKeyBindings.plist
 
+echo "Patching Xcode keybindings"
 sudo /usr/libexec/PlistBuddy -c "Delete :Custom" $XCODE_KEYBINDINGS_FILE
 sudo /usr/libexec/PlistBuddy -c "Merge $CUSTOM_KEYBINDINGS_FILE" $XCODE_KEYBINDINGS_FILE
+
+if test -e $XCODE_BETA_KEYBINDINGS_FILE
+  echo "Patching Xcode-beta keybindings"
+  sudo /usr/libexec/PlistBuddy -c "Delete :Custom" $XCODE_BETA_KEYBINDINGS_FILE
+  sudo /usr/libexec/PlistBuddy -c "Merge $CUSTOM_KEYBINDINGS_FILE" $XCODE_BETA_KEYBINDINGS_FILE
+end
