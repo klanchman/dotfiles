3faf9f76343934fd602792846b0975d310275b87
Kyle Lanchman <k.lanchman@gmail.com>
2020-10-14 14:15:46 -0400

    Support >2 Xcode installs

--- Extras/xcode/applyCustomBindings.fish
+++ Extras/xcode/applyCustomBindings.fish
@@ -1,15 +1,16 @@
 #!/usr/bin/env fish
 
-set XCODE_KEYBINDINGS_FILE "/Applications/Xcode.app/Contents/Frameworks/IDEKit.framework/Resources/IDETextKeyBindingSet.plist"
-set XCODE_BETA_KEYBINDINGS_FILE "/Applications/Xcode-beta.app/Contents/Frameworks/IDEKit.framework/Resources/IDETextKeyBindingSet.plist"
+set XCODE_KEYBINDINGS_RELPATH "Contents/Frameworks/IDEKit.framework/Resources/IDETextKeyBindingSet.plist"
 set CUSTOM_KEYBINDINGS_FILE (pwd)/Extras/xcode/CustomKeyBindings.plist
 
-echo "Patching Xcode keybindings"
-sudo /usr/libexec/PlistBuddy -c "Delete :Custom" $XCODE_KEYBINDINGS_FILE
-sudo /usr/libexec/PlistBuddy -c "Merge $CUSTOM_KEYBINDINGS_FILE" $XCODE_KEYBINDINGS_FILE
+set XCODE_APPS_DIR /Applications
+if test -d /Applications/Xcode
+  set XCODE_APPS_DIR /Applications/Xcode
+end
 
-if test -e $XCODE_BETA_KEYBINDINGS_FILE
-  echo "Patching Xcode-beta keybindings"
-  sudo /usr/libexec/PlistBuddy -c "Delete :Custom" $XCODE_BETA_KEYBINDINGS_FILE
-  sudo /usr/libexec/PlistBuddy -c "Merge $CUSTOM_KEYBINDINGS_FILE" $XCODE_BETA_KEYBINDINGS_FILE
+for f in (find "$XCODE_APPS_DIR" -maxdepth 1 -name "Xcode*.app")
+  echo "Patching keybindings in $f"
+  set XCODE_KEYBINDINGS_PATH "$f/$XCODE_KEYBINDINGS_RELPATH"
+  sudo /usr/libexec/PlistBuddy -c "Delete :Custom" $XCODE_KEYBINDINGS_PATH
+  sudo /usr/libexec/PlistBuddy -c "Merge $CUSTOM_KEYBINDINGS_FILE" $XCODE_KEYBINDINGS_PATH
 end
