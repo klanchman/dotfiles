31209a8661ccf68d1f1ebb081bcab0ac256934c7
Kyle Lanchman <k.lanchman@gmail.com>
2021-05-16 16:09:23 -0400

    Add itermoxyl

--- bin/setup.fish
+++ bin/setup.fish
@@ -8,3 +8,7 @@ end
 
 echo "Downloading cbor CLI tool..."
 curl -L https://github.com/fiatjaf/cbor/releases/download/v1.0.0/cbor_darwin_amd64 -o ~/bin/cbor
+
+echo "Downloading itermoxyl..."
+curl -L https://raw.githubusercontent.com/luciopaiva/itermoxyl/master/itermoxyl -o ~/bin/itermoxyl
+chmod u+x ~/bin/itermoxyl
