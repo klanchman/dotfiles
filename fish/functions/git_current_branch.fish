378fd607676b7ebc788100b830ae51fb69777f2d
Kyle Lanchman <k.lanchman@gmail.com>
2021-07-06 12:07:13 -0400

    Fix redirect in git function

--- fish/functions/git_current_branch.fish
+++ fish/functions/git_current_branch.fish
@@ -1,5 +1,5 @@
 function git_current_branch
-  set -l branch (git rev-parse --abbrev-ref HEAD ^/dev/null)
+  set -l branch (git rev-parse --abbrev-ref HEAD 2> /dev/null)
   if test -z $branch
     return
   end
