7bffc9c46e6dbbd5e581bd12e7c80c3674c63fb6
Kyle Lanchman <k.lanchman@gmail.com>
2021-09-13 13:07:20 -0400

    Add Xcode templates for Stevia

---
+++ xcode-templates/iOS/User Interface/Stevia View Controller.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,39 @@
+//___FILEHEADER___
+
+import Stevia
+import SwiftUI
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        makeHierarchy()
+    }
+}
+
+// MARK: - UI Setup
+extension ___FILEBASENAMEASIDENTIFIER___ {
+    // MARK: One Time Setup
+
+    private func makeHierarchy() {
+
+    }
+}
+
+// MARK: - Previews
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    static var previews: some View {
+        UIViewControllerPreview {
+            ___FILEBASENAMEASIDENTIFIER___()
+        }
+    }
+}
---
+++ xcode-templates/iOS/User Interface/Stevia View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,30 @@
+//___FILEHEADER___
+
+import Stevia
+import SwiftUI
+import UIKit
+
+class ___FILEBASENAMEASIDENTIFIER___: UIView {
+    init() {
+        super.init(frame: .zero)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+
+    }
+}
+
+// MARK: - Previews
+struct ___FILEBASENAMEASIDENTIFIER____Previews: PreviewProvider {
+    static var previews: some View {
+        UIViewPreview {
+            ___FILEBASENAMEASIDENTIFIER___()
+        }
+    }
+}
