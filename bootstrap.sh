7b3211f06417645d2d740ce7786b97c8111a1516
Kyle Lanchman <k.lanchman@gmail.com>
2021-10-26 22:24:43 -0400

    Update / fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,19 +7,19 @@ if ! command -v port > /dev/null; then
   exit 1
 fi
 
-if ! command -v port > /dev/null; then
+if ! command -v brew > /dev/null; then
   echo "Homebrew is not installed. Please install Homebrew first, then run this script again."
   exit 1
 fi
 
 echo "[sudo] Installing required ports..."
-sudo port install fish git rbenv ruby-build
+sudo port -N install fish git rbenv ruby-build
 
 echo "Installing required Homebrew packages..."
 brew install mint
 
 echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.38.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.0/install.sh | bash
 
 echo "[sudo] Adding fish to /etc/shells..."
 echo "/opt/local/bin/fish" | sudo tee -a /etc/shells
