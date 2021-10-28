eb82db50d2eab7288a93af5a4f475104b5c7023c
Kyle Lanchman <k.lanchman@gmail.com>
2021-10-27 23:49:58 -0400

    Add Silicon Homebrew path

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,4 @@
-fish_add_path ~/bin ~/.mint/bin /opt/local/bin /opt/local/sbin
+fish_add_path ~/bin ~/.mint/bin /opt/local/bin /opt/local/sbin /opt/homebrew/bin
 
 source ~/.config/fish/abbreviations.fish
 source ~/.config/fish/aliases.fish
