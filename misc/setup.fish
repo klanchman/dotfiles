2bafb1edd74c5f4330a6270836c851cf024e1a9e
Kyle Lanchman <k.lanchman@gmail.com>
2023-05-19 11:36:19 -0400

    Misc: add airdrop-cli

---
+++ misc/setup.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+echo "Installing airdrop-cli..."
+pushd $TMPDIR
+curl -L -o airdrop-cli.zip https://github.com/vldmrkl/airdrop-cli/archive/refs/heads/main.zip
+unzip -o airdrop-cli.zip
+pushd airdrop-cli-main
+make
+cp .build/release/airdrop ~/bin/airdrop
+popd
