fc5e744d1d8d0507fa7a7f60410418f7bc66d514
Kyle Lanchman <k.lanchman@gmail.com>
2023-08-18 18:36:23 -0400

    iTerm: use $HOME in paths

--- iterm/setup.fish
+++ iterm/setup.fish
@@ -2,5 +2,5 @@
 
 # Manually enable the Basic Python runtime and enable this script
 
-mkdir -p "/Users/klanchman/Library/Application Support/iTerm2/Scripts/AutoLaunch"
-ln -sf (pwd)/iterm/autoDarkMode.py "/Users/klanchman/Library/Application Support/iTerm2/Scripts/AutoLaunch/autoDarkMode.py"
+mkdir -p "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
+ln -sf (pwd)/iterm/autoDarkMode.py "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch/autoDarkMode.py"
