b391979c690ab6141896a46e15012220128b9ef1
Kyle Lanchman <k.lanchman@gmail.com>
2023-10-28 12:21:11 -0400

    brew: Update packages & config, use for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,26 +2,21 @@
 
 set -e
 
-if ! command -v port > /dev/null; then
-  echo "MacPorts is not installed. Please install MacPorts first, then run this script again."
-  exit 1
-fi
-
 if ! command -v brew > /dev/null; then
   echo "Homebrew is not installed. Please install Homebrew first, then run this script again."
   exit 1
 fi
 
-echo "[sudo] Installing required ports..."
-sudo port -N install fish git mint rbenv ruby-build
+echo "Installing required packages from Homebrew..."
+brew install fish git mint rbenv
 
 echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.4/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.39.5/install.sh | bash
 
 echo "[sudo] Adding fish to /etc/shells..."
-echo "/opt/local/bin/fish" | sudo tee -a /etc/shells
+echo "/opt/homebrew/bin/fish" | sudo tee -a /etc/shells
 
 echo "Changing default shell for user..."
-chsh -s /opt/local/bin/fish
+chsh -s /opt/homebrew/bin/fish
 
 echo "You are now ready to run setup.fish!"
--- brew/setup.fish
+++ brew/setup.fish
@@ -4,22 +4,60 @@ read -P "[H]ome or [W]ork? " WHERE_AM_I
 
 set BREWS \
   asimov \
+  bash \
+  bat \
+  carthage \
+  choose-rust \
+  cloc \
+  colima \
+  docker \
+  eza \
+  fd \
+  ffmpeg \
+  gh \
+  git-delta \
+  glab \
+  imagemagick \
+  inetutils \
+  jless \
+  jo \
+  jq \
+  mitmproxy \
+  qrencode \
+  shellcheck \
+  unxip \
+  xh \
+  xpdf \
+  yarn \
+  yq \
+  yt-dlp \
 
 set CASKS \
+  1password-cli \
   betterzip \
 
 if test "$WHERE_AM_I" = "H"
   set -a BREWS \
+    gpac \
+    hugo \
     mp3val \
+    xdelta \
+
+  set -a CASKS \
+    mkvtoolnix \
+    mpv \
 
 end
 
 if test "$WHERE_AM_I" = "W"
   set -a BREWS \
     azure-cli \
+    jmeter \
+    nginx \
 
   set -a CASKS \
     ngrok \
+    wireshark
 
 end
 
@@ -27,20 +65,20 @@ set CURRENT_BREWS (brew list --formula)
 set CURRENT_CASKS (brew list --cask)
 
 for BREW in $BREWS
-  if contains $BREW $CURRENT_BREWS
-    continue
+  if set INSTALLED_INDEX (contains -i $BREW $CURRENT_BREWS)
+    set -e BREWS[$INSTALLED_INDEX]
   end
-
-  brew install $BREW
 end
+
+brew install $BREWS
 
 for CASK in $CASKS
-  if contains $CASK $CURRENT_CASKS
-    continue
+  if set INSTALLED_INDEX (contains -i $CASK $CURRENT_CASKS)
+    set -e CASKS[$INSTALLED_INDEX]
   end
-
-  brew install --cask $CASK
 end
+
+brew install --cask $CASKS
 
 echo "Enabling Brew services, you may be prompted for your password"
 
