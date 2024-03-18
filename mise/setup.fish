95098922ffce3d0a0e9933bd8fcd28d1db6665c0
Kyle Lanchman <k.lanchman@gmail.com>
2024-03-17 23:43:01 -0400

    mise: Install Pythons

--- mise/setup.fish
+++ mise/setup.fish
@@ -5,6 +5,10 @@ mise completion fish > ~/.config/fish/completions/mise.fish
 
 mise plugins install -y dotnet
 
+mise install python 2.7 # For itermoxyl
+mise install python 3
+mise use -g python@3 python@2.7
+
 # You probably want to install some tools:
 # mise use -g node@<lts>
 # mise use -g ruby@<latest>
