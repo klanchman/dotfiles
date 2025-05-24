dc3da3413d070b423a85c4c1205551b3e9894d27
Kyle Lanchman <k.lanchman@gmail.com>
2025-05-23 23:01:19 -0400

    fish: restore old ctrl-c behavior

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 fish_add_path ~/bin ~/.mint/bin /opt/local/bin /opt/local/sbin /opt/homebrew/bin
 
+bind ctrl-c cancel-commandline
+
 source ~/.config/fish/abbreviations.fish
 source ~/.config/fish/aliases.fish
 source ~/.brewconfig
