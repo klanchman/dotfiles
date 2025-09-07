2dee209d875c75116c1a3d7d1c98392feceebe15
Kyle Lanchman <k.lanchman@gmail.com>
2025-09-07 15:48:07 -0400

    iTerm: remove dark mode script
    iTerm now has built-in support for changing the color scheme based on
    system dark mode setting

--- iterm/setup.fish
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env fish
-
-# Manually enable the Basic Python runtime and enable this script
-
-mkdir -p "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch"
-ln -sf (pwd)/iterm/autoDarkMode.py "$HOME/Library/Application Support/iTerm2/Scripts/AutoLaunch/autoDarkMode.py"
